91893899f589b04380eebbfc0f551b75212791a3
ROKAD VEDANT <111133257+rokadvednt03@users.noreply.github.com>
2024-02-19 19:07:05 +0530

    update ADC Driver

--- Inc/stm32f103xx_adc.h
+++ Inc/stm32f103xx_adc.h
@@ -1,125 +1,101 @@
 /**
-  ******************************************************************************
-  * @file    		stm32f103xx_adc.h
-  * @author  		Vedant A. Rokad
-  * @processor 	ARM Cortex-M3
-	* @controller STM32F103C8T6
-  * @date    		26-Feb-2022
-  * @brief   		Peripheral_Driver Header file
-  ******************************************************************************
- **/
-
+ * @file stm32f103xx_adc.h
+ * @brief Header file for STM32F103xx ADC
+ * @details This file contains functions to configure and control the ADC peripheral
+ *          on STM32F103xx microcontrollers.
+ *
+ * @author Vedant A. Rokad
+ * @date 3rd February 2024
+ * @board STM32F103C8T6 Bluepill
+ */
+ 
 #include "stm32f10x.h"
 
+/**
+ * @defgroup ADC_Configuration ADC Configuration
+ * @{
+ */
 
-/////////////////////////////////////////////////////////////////////////////////////////
-//                                                                                     //
-//				 															ADC_MACROS                           					 //
-//																															  										 //
-/////////////////////////////////////////////////////////////////////////////////////////
-#define ADC_Data_Align_RIGHT 0
-#define ADC_Data_Align_LEFT 1
+/** @brief ADC data alignment */
+#define ADC_DataAlign_RIGHT 0 /**< Right alignment for ADC data */
+#define ADC_DataAlign_LEFT 1  /**< Left alignment for ADC data */
 
-#define ADC_ScanConvMode_DI 0
-#define ADC_ScanConvMode_En 1
+/** @brief ADC sample time */
+#define ADC_SampleTime_1_5_Cycle   0 /**< 1.5 ADC clock cycles */
+#define ADC_SampleTime_7_5_Cycle   1 /**< 7.5 ADC clock cycles */
+#define ADC_SampleTime_13_5_Cycle  2 /**< 13.5 ADC clock cycles */
+#define ADC_SampleTime_28_5_Cycle  3 /**< 28.5 ADC clock cycles */
+#define ADC_SampleTime_41_5_Cycle  4 /**< 41.5 ADC clock cycles */
+#define ADC_SampleTime_55_5_Cycle  5 /**< 55.5 ADC clock cycles */
+#define ADC_SampleTime_71_5_Cycle  6 /**< 71.5 ADC clock cycles */
+#define ADC_SampleTime_239_5_Cycle 7 /**< 239.5 ADC clock cycles */
 
-#define ADC_RegularConvMode_SnglConv 0
-#define ADC_RegularConvMode_SeqnceConv 0
+/** @brief ADC channels */
+#define ADC_Channel_0  0  /**< ADC channel 0 */
+#define ADC_Channel_1  1  /**< ADC channel 1 */
+#define ADC_Channel_2  2  /**< ADC channel 2 */
+#define ADC_Channel_3  3  /**< ADC channel 3 */
+#define ADC_Channel_4  4  /**< ADC channel 4 */
+#define ADC_Channel_5  5  /**< ADC channel 5 */
+#define ADC_Channel_6  6  /**< ADC channel 6 */
+#define ADC_Channel_7  7  /**< ADC channel 7 */
+#define ADC_Channel_8  8  /**< ADC channel 8 */
+#define ADC_Channel_9  9  /**< ADC channel 9 */
+#define ADC_Channel_10 10 /**< ADC channel 10 */
+#define ADC_Channel_11 11 /**< ADC channel 11 */
+#define ADC_Channel_12 12 /**< ADC channel 12 */
+#define ADC_Channel_13 13 /**< ADC channel 13 */
+#define ADC_Channel_14 14 /**< ADC channel 14 */
+#define ADC_Channel_15 15 /**< ADC channel 15 */
+#define ADC_Channel_16 16 /**< ADC channel 16 */
+#define ADC_Channel_17 17 /**< ADC channel 17 */
 
-#define ADC_ContConvMode_ContinuousMode 0
-#define ADC_ContConvMode_DisContinuousMode 1
+/** @} */
 
-#define ADC_Channel_0		0
-#define ADC_Channel_1		1
-#define ADC_Channel_2		2
-#define ADC_Channel_3		3
-#define ADC_Channel_4		4
-#define ADC_Channel_5		5
-#define ADC_Channel_6		6
-#define ADC_Channel_7		7
-#define ADC_Channel_8		8
-#define ADC_Channel_9		9
-#define ADC_Channel_10		10
-#define ADC_Channel_11		11
-#define ADC_Channel_12		12
-#define ADC_Channel_13		13
-#define ADC_Channel_14		14
-#define ADC_Channel_15		15
+/**
+ * @struct ADC_Config_t
+ * @brief Structure to hold ADC configuration parameters
+ */
+typedef struct {
+    uint32_t ADC_DataAlign;    /**< Data alignment for ADC */
+    uint32_t ADC_Channel;      /**< ADC channel */
+    uint32_t ADC_SampleTime;   /**< Sample time for ADC */
+} ADC_Config_t;
 
+/**
+ * @struct ADC_Handle_t
+ * @brief Structure to hold ADC handle and configuration
+ */
+typedef struct {
+    ADC_TypeDef *pADCx;        /**< Pointer to ADC peripheral */
+    ADC_Config_t ADCConfig;    /**< ADC configuration */
+} ADC_Handle_t;
 
-#define ADC_SmplRate_1_5_Cycle 0
-#define ADC_SmplRate_7_5_Cycle 1
-#define ADC_SmplRate_13_5_Cycle 2
-#define ADC_SmplRate_28_5_Cycle 3
-#define ADC_SmplRate_41_5_Cycle 4
-#define ADC_SmplRate_55_5_Cycle 5
-#define ADC_SmplRate_71_5_Cycle 6
-#define ADC_SmplRate_239_5_Cycle 7
+/**
+ * @brief Function to control the peripheral clock of ADC
+ * @param pADCx Pointer to the ADC peripheral
+ * @param EnorDi Enable or disable the peripheral clock (1: enable, 0: disable)
+ * @retval None
+ */
+void ADC_PeriClockControl(ADC_TypeDef *pADCx, uint32_t EnorDi);
 
-#define ADC_Ch_sqn_1 0
-#define ADC_Ch_sqn_2 1
-#define ADC_Ch_sqn_3 2
-#define ADC_Ch_sqn_4 3
-#define ADC_Ch_sqn_5 4
-#define ADC_Ch_sqn_6 5
-#define ADC_Ch_sqn_7 6
-#define ADC_Ch_sqn_8 7
-#define ADC_Ch_sqn_9 8
-#define ADC_Ch_sqn_10 9
-#define ADC_Ch_sqn_11 10
-#define ADC_Ch_sqn_12 11
-#define ADC_Ch_sqn_13 12
-#define ADC_Ch_sqn_14 13
-#define ADC_Ch_sqn_15 14
-#define ADC_Ch_sqn_16 15
-#define ADC_Ch_sqn_17 16
-#define ADC_Ch_sqn_18 17
+/**
+ * @brief Function to initialize ADC for single channel conversion
+ * @param pADCHandler Pointer to ADC handle structure
+ * @retval None
+ */
+void ADC_Init_SingleChannel(ADC_Handle_t *pADCHandler);
 
+/**
+ * @brief Function to start ADC conversion
+ * @param pADCHandler Pointer to ADC handle structure
+ * @retval None
+ */
+void ADC_StartConversion(ADC_Handle_t *pADCHandler);
 
-
-/////////////////////////////////////////////////////////////////////////////////////////
-//                                                                                     //
-// 															 ADC_Configuration_Structure                           //
-//																															  										 //
-/////////////////////////////////////////////////////////////////////////////////////////
-typedef struct
-{
-	uint32_t Data_Align ;
-	uint32_t NoOFConvMode;
-	uint32_t RegularConvMode;
-	uint32_t SamplingRate;
-	/*uint32_t ScanConvMode;
-	uint32_t NoOFConvMode;
-	uint32_t RegularConvMode;
-	uint32_t ContConvMode;
-	*/
-}ADC_Config_t;
-
-
-/////////////////////////////////////////////////////////////////////////////////////////
-//                                                                                     //
-//																 ADC_Handling_Structure                              //
-//																															  										 //
-/////////////////////////////////////////////////////////////////////////////////////////
-typedef struct
-{
-	ADC_Config_t Config;
-	ADC_TypeDef *pADCx;
-	
-}ADC_Handle_t;
-
-
-
-
-/////////////////////////////////////////////////////////////////////////////////////////
-//                                                                                     //
-//												  	GPIO_APIs_&_HELPING_FUNCTION                             //
-//																															  										 //
-/////////////////////////////////////////////////////////////////////////////////////////
-void ADC_Init(ADC_Handle_t *pHandleADC);
-void ADC_PeriClockControl(ADC_TypeDef *pADCx , uint8_t EnorDI);
-void ADC_OneChannel(ADC_Handle_t *pHandleADC , uint8_t channel_name);
-void ADC_StartConv(ADC_TypeDef *pADCx);
-void ADC_Data_Align(ADC_Handle_t *pHandleADC);
-void ADC_ChConf(ADC_Handle_t *pHandleADC , uint8_t channel_name,uint8_t channel_sequence);
-
+/**
+ * @brief Function to read ADC conversion result
+ * @param pADCHandler Pointer to ADC handle structure
+ * @retval 16-bit ADC conversion result
+ */
+uint16_t ADC_ReadConversion(ADC_Handle_t *pADCHandler);
--- Src/stm32f103xx_adc.c
+++ Src/stm32f103xx_adc.c
@@ -1,132 +1,117 @@
 /**
-  ******************************************************************************
-  * @file    		stm32f103xx_adc.c
-  * @author  		Vedant A. Rokad
-  * @processor 	ARM Cortex-M3
-	* @controller STM32F103C8T6
-  * @date    		26-Feb-2023
-  * @brief   		Peripheral_Driver Source file
-  ******************************************************************************
- **/
-
+ * @file stm32f103xx_adc.c
+ * @brief Header file for STM32F103xx ADC
+ * @details This file contains functions to configure and control the ADC peripheral
+ *          on STM32F103xx microcontrollers.
+ *
+ * @author Vedant A. Rokad
+ * @date 3rd February 2024
+ * @board STM32F103C8T6 Bluepill
+ */
+ 
 #include "stm32f103xx_adc.h"
 
+/**
+ * @brief Function to control the peripheral clock of ADC
+ * @param pADCx Pointer to the ADC peripheral
+ * @param EnorDi Enable or disable the peripheral clock (ENABLE: enable, DISABLE: disable)
+ * @retval None
+ */
+void ADC_PeriClockControl(ADC_TypeDef *pADCx,uint32_t EnorDi){
+		if(EnorDi == ENABLE)
+		{			
+			if(pADCx == ADC1) 
+			{
+				RCC->APB2ENR |= (RCC_APB2ENR_ADC1EN);
+			}
+			else if(pADCx == ADC2)
+			{
+				RCC->APB2ENR |= (RCC_APB2ENR_ADC2EN);				
+			}
+		}
+		else
+		{
+			if(pADCx == ADC1) 
+			{
+				RCC->APB2ENR |= ~(RCC_APB2ENR_ADC1EN);
+			}
+			else if(pADCx == ADC2)
+			{
+				RCC->APB2ENR |= ~(RCC_APB2ENR_ADC2EN);				
+			}
+		}
+}
 
-/////////////////////////////////////////////////////////////////////////////////////////
-//                                                                                     //
-//																 ADC_PeriClockControl                                //
-//																															  										 //
-/////////////////////////////////////////////////////////////////////////////////////////
-void ADC_PeriClockControl(ADC_TypeDef *pADCx , uint8_t EnorDI)
-{
-	if(EnorDI == ENABLE)
-	{
-		if(pADCx == ADC1)
-			RCC->APB2ENR |= (RCC_APB2ENR_ADC1EN);
-		else 
-			RCC->APB2ENR |= (RCC_APB2ENR_ADC1EN);
+
+/**
+ * @brief Function to initialize ADC for single channel conversion
+ * @param pADCHandler Pointer to ADC handle structure
+ * @retval None
+ */
+void ADC_Init_SingleChannel(ADC_Handle_t *pADCHandler){
+
+	ADC_PeriClockControl(pADCHandler->pADCx,ENABLE);
+/**
+	* @brief: Configure Data Align
+	*/
+	if(pADCHandler->ADCConfig.ADC_DataAlign == ADC_DataAlign_RIGHT){
+			pADCHandler->pADCx->CR2 &= ~(ADC_CR2_ALIGN);
 	}
-	else
-	{
-		if(pADCx == ADC1)
-			RCC->APB2ENR &= ~(RCC_APB2ENR_ADC1EN);
-		else 
-			RCC->APB2ENR &= ~(RCC_APB2ENR_ADC1EN);
+	else{
+			pADCHandler->pADCx->CR2 |= (ADC_CR2_ALIGN);
 	}
-}
 
+/**
+	* @brief: Configure Continuious Mode
+	*/	
+	pADCHandler->pADCx->CR2 |= (ADC_CR2_CONT) ;
 
+/**
+	* @brief: Configure External Event on to the Software event on
+	*/		
+	pADCHandler->pADCx->CR2 |= (ADC_CR2_EXTSEL);
 
-/////////////////////////////////////////////////////////////////////////////////////////
-//                                                                                     //
-//													ADC_One_Channel_Conversion_Init                            //
-//																															  										 //
-/////////////////////////////////////////////////////////////////////////////////////////
-void ADC_OneChannel(ADC_Handle_t *pHandleADC , uint8_t channel_name)
-{
-		if(pHandleADC->Config.Data_Align == ADC_Data_Align_RIGHT)
-			pHandleADC->pADCx->CR2 &= ~ADC_CR2_ALIGN;
-		else
-			pHandleADC->pADCx->CR2 |= ADC_CR2_ALIGN;
-		
-		
-		//config sampling rate
-		if(channel_name < ADC_Channel_10)
-		{
-			//config in ADC_SMPR2
-			pHandleADC->pADCx->SMPR2 |= (pHandleADC->Config.SamplingRate << (channel_name)*3);
-		}
-		else
-		{
-			//config in ADC_SMPR1
-			pHandleADC->pADCx->SMPR2 |= (pHandleADC->Config.SamplingRate << (channel_name*3) - 30);
-		}
-		
-		//config sequence
-		pHandleADC->pADCx->SQR3 |= (channel_name);
+/**
+	* @brief: Configure Sample Cycle
+	*/			
+	if(pADCHandler->ADCConfig.ADC_Channel <= ADC_Channel_9){
+		pADCHandler->pADCx->SMPR2 |= (uint32_t)(pADCHandler->ADCConfig.ADC_Channel*3);
+	}
+	else{
+		pADCHandler->pADCx->SMPR2 |= (uint32_t)((pADCHandler->ADCConfig.ADC_Channel-10)*3);
+	}
+	
+/**
+	* @brief: Configure Sequence
+	*/				
+	pADCHandler->pADCx->SQR3 |= (pADCHandler->ADCConfig.ADC_Channel);
 	
 }
 
 
-/////////////////////////////////////////////////////////////////////////////////////////
-//                                                                                     //
-// 																	ADC_Start_Conersion                                //
-//																															  										 //
-/////////////////////////////////////////////////////////////////////////////////////////
-void ADC_StartConv(ADC_TypeDef *pADCx)
-{
-	int a ;
-	pADCx->CR1 |= (ADC_CR1_SCAN);
-	pADCx->CR2 |= (ADC_CR2_CONT);
-	pADCx->CR2 |= (ADC_CR2_ADON);
-	for(a=0 ; a<10000;a++);
-	pADCx->CR2 |= (ADC_CR2_ADON);
-	for(a=0 ; a<10000;a++);
-	for(a=0 ; a<10000;a++);
-	pADCx->CR2 |= ADC_CR2_CAL;
-	for(a=0 ; a<10000;a++);
-	for(a=0 ; a<10000;a++);
+/**
+ * @brief Function to start ADC conversion
+ * @param pADCHandler Pointer to ADC handle structure
+ * @retval None
+ */
+void ADC_StartConversion(ADC_Handle_t *pADCHandler){
+		
+		pADCHandler->pADCx->CR2 |= (ADC_CR2_RSTCAL);
+		while((pADCHandler->pADCx->CR2) & (ADC_CR2_RSTCAL));
+		pADCHandler->pADCx->CR2 |= (ADC_CR2_CAL);
+		while((pADCHandler->pADCx->CR2) & (ADC_CR2_CAL));
+		
+	
+		pADCHandler->pADCx->CR2 |= (ADC_CR2_ADON) ;
+		pADCHandler->pADCx->CR2 |= (ADC_CR2_EXTTRIG) ;
+		pADCHandler->pADCx->CR2 |= (ADC_CR2_SWSTART) ;	
 }
 
-
-void ADC_Init(ADC_Handle_t *pHandleADC)
-{
-	if(pHandleADC->Config.Data_Align == ADC_Data_Align_RIGHT)
-		pHandleADC->pADCx->CR2 &= ~ADC_CR2_ALIGN;
-	else
-		pHandleADC->pADCx->CR2 |= ADC_CR2_ALIGN;
-	
-	pHandleADC->pADCx->SQR1 |= (pHandleADC->Config.NoOFConvMode-1 << 20);
+/**
+ * @brief Function to read ADC conversion result
+ * @param pADCHandler Pointer to ADC handle structure
+ * @retval 16-bit ADC conversion result
+ */
+uint16_t ADC_ReadConversion(ADC_Handle_t *pADCHandler){
+	return (uint16_t)(pADCHandler->pADCx->DR & (0xffff));
 }
-	
-void ADC_ChConf(ADC_Handle_t *pHandleADC , uint8_t channel_name,uint8_t channel_sequence)
-{
-		
-		//config sampling rate
-		if(channel_name < ADC_Channel_10)
-		{
-			//config in ADC_SMPR2
-			pHandleADC->pADCx->SMPR2 |= (pHandleADC->Config.SamplingRate << (channel_name)*3);
-		}
-		else
-		{
-			//config in ADC_SMPR1
-			pHandleADC->pADCx->SMPR2 |= (pHandleADC->Config.SamplingRate << (channel_name*3) - 30);
-		}
-		
-		//config sequence
-		if(channel_sequence < ADC_Ch_sqn_7)
-		{
-			pHandleADC->pADCx->SQR3 |= (channel_name << (channel_sequence*5));
-		}
-		else if((channel_sequence < ADC_Ch_sqn_13) & (channel_sequence > ADC_Ch_sqn_6))
-		{
-			pHandleADC->pADCx->SQR3 |= (channel_name << ((channel_sequence*5)-35));
-		}
-		else if((channel_sequence > ADC_Ch_sqn_12))
-		{
-			pHandleADC->pADCx->SQR3 |= (channel_name << ((channel_sequence*5)-65));
-		}
-	
-}
-	
